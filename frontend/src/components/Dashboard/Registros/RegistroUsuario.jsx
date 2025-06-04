e61a45990ef843aa5653d746f1134f31d02180f5
al211185 <al211185@alumnos.uacj.mx>
2025-06-03 23:54:46 -0600

    ajuste de registro de usuarios

--- frontend/src/components/Dashboard/Registros/RegistroUsuario.jsx
+++ frontend/src/components/Dashboard/Registros/RegistroUsuario.jsx
@@ -12,6 +12,7 @@ export default function RegistroUsuario() {
     email: '',
     password: '',
     telefono: '',
+    empleadoID: '',
     departamento: '',
     rol: '',
   });
@@ -108,6 +109,12 @@ export default function RegistroUsuario() {
             label="Teléfono"
             name="telefono"
             value={userData.telefono}
+            onChange={handleChange}
+          />
+          <Input
+            label="Empleado ID"
+            name="empleadoID"
+            value={userData.empleadoID}
             onChange={handleChange}
           />
 
